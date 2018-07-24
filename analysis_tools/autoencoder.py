dfd6f16373e49704b9239f95de5e2d44101d7471
Ryan Jadrich <ryanj@curie.che.utexas.edu>
2018-07-24 13:02:55 -0500

    updated autoencoder to use multiple probes

--- analysis_tools/autoencoder.py
+++ analysis_tools/autoencoder.py
@@ -13,29 +13,35 @@ class AutoEncoder:
                  **kwargs):
         
         self.layer_dims = []
-        data = Input(shape=(dim,))       
+        self.encoded_layer = 0
+        encode_data = Input(shape=(dim,)) 
+        decode_data = Input(shape=(encode_dim,))
 
         #initial expansion layer
         start_dim = int(dim*(1.0 + initial_growth))
-        encoded = Dense(start_dim, **kwargs)(data)
+        encoded = Dense(start_dim, **kwargs)(encode_data)
         self.layer_dims.append(start_dim)
+        self.encoded_layer += 1
 
         #compression layers
         current_dim = int(start_dim*shrink)
         while current_dim > encode_dim:
             encoded = Dense(current_dim, **kwargs)(encoded)
             self.layer_dims.append(current_dim)
+            self.encoded_layer += 1
             current_dim = int(current_dim*shrink)
         
         #final encoding layer
         #encoded = Dense(encode_dim, **kwargs)(encoded)
         encoded = Dense(encode_dim, activation='linear')(encoded) 
         self.layer_dims.append(encode_dim)
+        self.encoded_layer += 1
         
         #first expansion layer
         reversed_dims = self.layer_dims[::-1][1:]
-        decoded = Dense(reversed_dims[0], **kwargs)(encoded)
+        decoded = Dense(reversed_dims[0], **kwargs)(decode_data)
         self.layer_dims.append(reversed_dims[0])
+        self.encoded_layer += 1
         
         #remaining expansion layers
         for current_dim in reversed_dims[1:]:
@@ -46,9 +52,14 @@ class AutoEncoder:
         decoded = Dense(dim, activation='linear')(decoded)
         self.layer_dims.append(dim)
         
-        #save the models
-        self.autoencoder = Model(data, decoded)
-        self.encoder = Model(data, encoded)
+        #create encoder and decoder models
+        self.encoder = Model(encode_data, encoded)
+        self.decoder = Model(decode_data, decoded)
+        
+        #create the joint model
+        _encoded_ = self.encoder(encode_data)
+        _decoded_ = self.decoder(_encoded_)
+        self.autoencoder = Model(encode_data, _decoded_)
         
         return None
     
@@ -66,8 +77,11 @@ class AutoEncoder:
                                         validation_data=(features_test, features_test), 
                                         **kwargs)
         
-    def Predict(self, features, **kwargs):
+    def Encode(self, features, **kwargs):
         return self.encoder.predict(features, **kwargs)
+    
+    def Decode(self, features, **kwargs):
+        return self.decoder.predict(features, **kwargs)
     
     def Summary(self, **kwargs):
         return self.autoencoder.summary(**kwargs)
